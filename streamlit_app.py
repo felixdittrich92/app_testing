d1b71c4bebb45252f2ecae40f5166fd14e3ddaf1
Felix Dittrich <felixdittrich92@gmail.com>
2020-11-20 08:46:06 +0100

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ def __get_text_from_image(image):
   text = text.decode('utf8')
   return text
 
-st.title("denoise and evaluate images")
+st.title("denoise and evaluate scanned document images")
 img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 model_eval, model_auto = load_models()
@@ -76,7 +76,7 @@ if img_file_buffer is not None:
 
     org = load_img(temp_file.name)
     y_pred_class, score = __predict_score(temp_file.name)
-   # text = __get_text_from_image(img_file_buffer)
+    text = __get_text_from_image(temp_file.name)
 
     st.image(org, caption=f"Original", width=700)
     st.write("Predicted class : %s" % (CLASS_IDXS[y_pred_class]))
@@ -84,10 +84,10 @@ if img_file_buffer is not None:
    # st.write(text)
 
     img = __auto_encode(temp_file.name)
-    #file_object = io.BytesIO()
-    #img.save(file_object, 'PNG')
+    file_object = io.BytesIO()
+    img.save(file_object, 'PNG')
     temp_file = NamedTemporaryFile(delete=False)
-    temp_file.write(img)
+    temp_file.write(file_object)
     y_pred_class, score = __predict_score(temp_file)
    # text = __get_text_from_image(file_object)
 
