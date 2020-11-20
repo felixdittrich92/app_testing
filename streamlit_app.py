5f843493cf8005e9f2fdd4651621e6d6235bb935
Felix Dittrich <felixdittrich92@gmail.com>
2020-11-20 09:54:01 +0100

    final

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"
 
 model_eval, model_auto = load_models()
 
-image = Image.open('images/doc.jpg')
+image = np.array(Image.open('images/doc.jpg'))
 app(image)
 
 if img_file_buffer is not None:
