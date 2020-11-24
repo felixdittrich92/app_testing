be093fa98234b761f8ee923e14cd871448432fcb
Felix <felixdittrich92@gmail.com>
2020-11-24 11:25:58 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ def __get_text_from_image_ocrmypdf(image):
     txt = str(t) + '/1.txt'
     ocrmypdf.ocr(input_file=image, 
                  output_file=pdfa, 
-                 language='deu',
+                 language=['deu', 'eng'],
                  force_ocr=True, 
                  deskew=True,
                  optimize=1, 
